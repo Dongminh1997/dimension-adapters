c44582a9ed09a6cbf6a1c5fe307efbaa6f904ed6
Dongminh1997 <minhquanghaidong@gmail.com>
2024-12-27 20:09:32 +0700

    Adding SONIC and ZKSYNC network

--- aggregators/magpie/index.ts
+++ aggregators/magpie/index.ts
@@ -104,7 +104,7 @@ const fetch = async (timestamp: number, _: ChainBlocks, {chain}: FetchOptions): 
           start: 1662595200,
         },
       },
-      // isExpensiveAdapter: true,
+      isExpensiveAdapter: true,
     };
 
 export default adapter;
